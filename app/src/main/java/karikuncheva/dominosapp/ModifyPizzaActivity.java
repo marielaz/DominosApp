91344efa2e02b3c86367b7c8c3b44e1614290432
marielaz <m.zviskova11@gmail.com>
2017-03-17 13:27:55 +0200

    modify

--- app/src/main/java/karikuncheva/dominosapp/ModifyPizzaActivity.java
+++ app/src/main/java/karikuncheva/dominosapp/ModifyPizzaActivity.java
@@ -2,12 +2,40 @@ package karikuncheva.dominosapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModifyPizzaActivity extends AppCompatActivity {
+
+    List<RadioButton> radioButtons = new ArrayList<RadioButton>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_pizza);
+
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton));
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton2));
+        radioButtons.add((RadioButton) findViewById(R.id.radioButton3));
+
+        for (RadioButton button : radioButtons) {
+            button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) processRadioButtonClick(buttonView);
+                }
+
+            });
+        }
+    }
+
+    private void processRadioButtonClick(CompoundButton buttonView) {
+        for (RadioButton button : radioButtons) {
+            if (button != buttonView) button.setChecked(false);
+        }
+
     }
 }
